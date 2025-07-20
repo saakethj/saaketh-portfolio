330741522e73bf8f1b5fd3d9a82a5bc8d5bcfecd
Saaketh <saaketh.jella123@gmail.com>
2025-07-20 23:39:54 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="bg-black min-h-screen text-white">
+    <div className="min-h-screen w-screen overflow-x-hidden bg-black text-white">
       <Navbar />
+      <Hero />
     </div>
   );
 }
