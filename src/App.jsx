9e0f064810ed6b761b514a187d8ef8c22ab07b82
Saaketh <saaketh.jella123@gmail.com>
2025-07-24 00:43:13 +0530

    DEploying to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AboutText from "./components/About";
 import Experience from "./components/Experience";
-import SkillsSection from './components/SkillsSection'
+import SkillsSection from './components/SkillsSection';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <div className="min-h-screen w-screen overflow-x-hidden bg-black text-white">
+      <SpeedInsights />
       <Navbar />
       <Hero />
       <AboutText />
